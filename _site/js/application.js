94712e5fe74eb5a9a94bd9847e339dc55de071ed
Sukjoon Kim <sjoonk@gmail.com>
2011-10-31 17:49:30 +0900

    post a blog

---
+++ _site/js/application.js
@@ -0,0 +1,43 @@
+/* Author: 
+
+*/
+
+$(function() {
+	$('.bigger').biggerlink();
+	$('.tabs').tabs();
+
+	// $('#jTweetsAnywhere').jTweetsAnywhere({
+	//    username: 'usefulparadigm',
+	//    count: 5,
+	//    showTweetFeed: {
+	// 	showProfileImages: true,
+	//         // showUserScreenNames: true,
+	//        	paging: { mode: 'more' }
+	//        },
+	//    tweetFilter: function(tweet, options) { 
+	// 		return !tweet.text.match(/^@.+/);
+	//    },
+	//    showFollowButton: false
+	// });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
