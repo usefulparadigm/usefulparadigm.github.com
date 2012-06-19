d06580ff8aa8ee219d5e7ac0aef20f50523fede1
Sukjoon Kim <sjoonk@gmail.com>
2012-06-19 16:22:31 +0900

    add fancybox to google map

--- js/application.js
+++ js/application.js
@@ -6,6 +6,18 @@ $(function() {
 	$('.bigger').biggerlink();
 	$('.tabs').tabs();
 
+	$(".various").fancybox({
+		// maxWidth	: 800,
+		// maxHeight	: 600,
+		// fitToView	: false,
+		// width		: '70%',
+		// height		: '70%',
+		// autoSize	: false,
+		// closeClick	: false,
+		// openEffect	: 'none',
+		// closeEffect	: 'none'
+	});
+
 	// $('#jTweetsAnywhere').jTweetsAnywhere({
 	//    username: 'usefulparadigm',
 	//    count: 5,
